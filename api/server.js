9c90ffc09be2dd3d13eaf1cc36b98d3723e70806
Alec Dye <alecdye@gmail.com>
2020-04-20 20:45:17 -0700

    Building authentication router

--- api/server.js
+++ api/server.js
@@ -3,9 +3,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
-const Users = require("../users/users-model.js");
-
+// Routes
 const usersRouter = require("../users/users-router");
+const authRouter = require("../auth/auth-router");
 
 const server = express();
 
@@ -13,24 +13,11 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "live" });
 });
-
-server.post("/api/register", (req, res) => {
-  let user = req.body;
-
-  Users.add(user)
-    .then((saved) => {
-      res.status(201).json(saved);
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .json({ errorMessage: "Failed to submit registration", error });
-    });
-});
-
-server.post("/api/login", (req, res) => {});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,6 +5,10 @@ const Users = require("../users/users-model.js");
 
 router.post("/register", (req, res) => {
   let user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 12;
+
+  const hash = bcrypt.hashSync(user.password, rounds);
+  user.password = hash;
 
   Users.add(user)
     .then((saved) => {
@@ -17,6 +21,21 @@ router.post("/register", (req, res) => {
     });
 });
 
-server.post("/api/login", (req, res) => {});
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        res.status(200).json({ message: "Welcome!" });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
 
 module.exports = router;
