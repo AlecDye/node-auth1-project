9e35eec1e44b532e3cb079e793de6d2b3be4d1ef
Alec Dye <alecdye@gmail.com>
2020-04-20 20:10:28 -0700

    Building database migrations

--- api/server.js
+++ api/server.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/users-model.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.json({ api: "live" });
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ errorMessage: "Failed to submit registration", error });
+    });
+});
+
+server.post("/api/login", (req, res) => {});
 
 module.exports = server;
